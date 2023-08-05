d823ffa7c03c7cec6ebbd72158ed9a76fb016968
MoZoLbKA <makurin.nik@list.ru>
2023-08-05 16:18:53 +0300

    Add project files.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,14 @@
+﻿namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditTime { get; set; }
+
+
+    }
+}
---
+++ Notes.Persistance/EntityTipeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+
+namespace Notes.Persistance.EntityTipeConfiguration
+{
+    internal class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id).IsUnique();
+            builder.Property(note => note.Title).HasMaxLength(256);
+
+        }
+    }
+}
