afabc1a1f6207e49ee67461697bdd0ac76bf3f36
MoZoLbKA <makurin.nik@list.ru>
2023-08-05 17:22:44 +0300

    Created Mapping [Update]

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Common.Mappings
+{
+    internal interface IMapWith
+    {
+    }
+}
